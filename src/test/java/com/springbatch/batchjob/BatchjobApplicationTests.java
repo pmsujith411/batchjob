6472a8e263efa343065eae3780beeaf0ea395f3c
sujith <neethusujithpm@outlook.com>
2021-07-01 11:34:45 +0530

    initial commit

---
+++ src/test/java/com/springbatch/batchjob/BatchjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbatch.batchjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
